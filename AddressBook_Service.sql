94d3f5067ae5a95a64aab224dcb47eb3e6d253ff
Minal <patilminalp@gmail.com>
2021-07-23 23:35:00 +0530

    Added:Create AddressBook-Table using mySql

--- AddressBook_Service.sql
+++ AddressBook_Service.sql
@@ -1,3 +1,19 @@
+#Uc1
 create database AddressBook_Service;
 show databases;
 use AddressBook_Service;
+
+#Uc2
+create table AddressBook(
+	id int unsigned not null auto_increment,
+	first_name varchar (15) not null,
+    last_name varchar (15) not null,
+    address varchar (150) not null,
+	city varchar (150) not null,
+	state varchar (150) not null,
+	zip int not null,
+    phone_number long not null,
+    email varchar (100) not null,
+    primary key (id)
+
+);
